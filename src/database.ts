882649f1fd5c42ff8653a4c0d1237c4b314f9768
amribrahim34 <amramr3434@gmail.com>
2022-01-31 10:08:51 +0200

    preparing tests

--- src/database.ts
+++ src/database.ts
@@ -7,14 +7,29 @@ const {
     POSTGRES_HOST,
     POSTGRES_DB,
     POSTGRES_USER,
-    POSTGRES_PASSWORD
+    POSTGRES_PASSWORD,
+    POSTGRES_TEST_DB,
+    APP_ENV
 } = process.env
 
-const Client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-})
+let Client: Pool;
 
-export default Client;
+if (APP_ENV == 'testing') {
+    Client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    })
+} else {
+    Client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+    })
+}
+
+
+
+export default Client;  
