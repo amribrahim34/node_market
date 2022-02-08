a06c278c51e70d7b9a845442e1a70b833ec1a13a
amribrahim34 <amramr3434@gmail.com>
2022-02-08 10:48:13 +0200

    category index and create are working fine

--- src/models/category.ts
+++ src/models/category.ts
@@ -2,17 +2,16 @@ import { Connection } from 'pg';
 import Client from '../database';
 
 type CategoryType = {
-    id: Number;
+    id?: Number;
     name: String;
 }
 
 class Category {
-  async create(n: String): Promise<any> {
+  static async create(n: string): Promise<any> {
     try {
       const con = await Client.connect();
-      const sql = `INSERT INTO categories ('name') VALUES(${n})`;
-      await con.query(sql);
-      const result = await con.query(`SELECT * FROM categories WHERE name = ${n}`);
+      const sql = `INSERT INTO categories (name) VALUES($1) RETURNING *`;
+      const result = await con.query(sql,[n]);
       con.release();
       return result.rows;
     } catch (error) {
@@ -25,9 +24,11 @@ class Category {
       const con = await Client.connect();
       const sql = 'SELECT * FROM categories';
       const result = await con.query(sql);
+      console.log(result);
       con.release();
       return result.rows;
     } catch (error) {
+      console.log(`the error is ${error}`)
       throw new Error(`cannot get categories ${error}`);
     }
   }
--- src/routes/api/categories.ts
+++ src/routes/api/categories.ts
@@ -2,15 +2,34 @@ import express from 'express';
 import { Category, CategoryType } from '../../models/category';
 
 const categories = express.Router();
-const cat = new Category;
 
+/**
+ * methode to get all categories from the database 
+ * @method get
+ * @return Categories
+ */
 categories.get('/', (req: express.Request, res: express.Response): void => {
-  const result = async ()=>{
-    await Category.index;
-  }
-  console.log(result);
-  res.send(result);
-//   res.end();
+    Category.index().then((result)=>{
+      res.json(result);
+    }).catch((error) => {
+      res.send(error);
+    });
 });
+
+/**
+ * methode to insert new category to the database 
+ * @method POST
+ * @param Name
+ * @return Category
+ */
+categories.post('/create', (req: express.Request, res: express.Response): void => {
+  Category.create('cat1').then((result)=>{
+    res.json(result);
+  }).catch((error) => {
+    res.send(error);
+  });
+});
+
+
 
 export default categories;
