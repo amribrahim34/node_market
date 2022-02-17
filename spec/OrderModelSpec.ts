000d91e0ae1ab4696f8ac3d13b2d6beb1563ffef
amribrahim34 <amramr3434@gmail.com>
2022-02-17 09:22:52 +0200

    order index

--- spec/OrderModelSpec.ts
+++ spec/OrderModelSpec.ts
@@ -45,6 +45,33 @@ describe('order Model', () => {
   });
 
   it('index method should return a list of categories', async () => {
+    const userArray = {
+      "first_name" : 'Amr',
+      "last_name" : 'Ibrahim',
+      "email":'mail@mail.com',
+      "password":"password"
+    }
+    const category = await Category.create("name");
+    const catId = Number( category[0].id)
+    const productArray = {
+        "name":"product 1",
+        "details": "details",
+        "price" : 125,
+        "category_id":catId as number
+    }
+    
+    const product = await ProductModel.create(productArray);
+    const prodId = Number( product[0].id)
+    const user = await UserModel.create(userArray);
+    const usrId = Number( user[0].id)
+    const orderArray :ComingOrderType= {
+        "user_id": usrId as number,
+        "products": [{
+            "product_id": prodId as number,
+            "quantity":  5
+        }],
+    }
+    const order = await OrderModel.create(orderArray);
     const result = await OrderModel.index();
     expect(result.length).toBeGreaterThan(0);
   });
